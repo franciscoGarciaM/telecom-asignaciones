d1c2abbda4bb2f0de9fb9df79c89631cc499ebe2
TrainibIT 05 <trainibit05@TrainibITs-MacBook-Pro.local>
2024-08-28 11:59:32 -0600

    First commit add git ignore

---
+++ src/main/java/com/telecom/asignaciones/AsignacionesApplication.java
@@ -0,0 +1,13 @@
+package com.telecom.asignaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsignacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsignacionesApplication.class, args);
+	}
+
+}
