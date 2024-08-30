4f30cab6a9185ee2ac297cc2ebe355a1c39e2e07
TrainibIT 05 <trainibit05@TrainibITs-MacBook-Pro.local>
2024-08-30 14:04:12 -0600

    service y controller

--- src/main/java/com/telecom/asignaciones/Response/AsignacionResponse.java
+++ src/main/java/com/telecom/asignaciones/Response/AsignacionResponse.java
@@ -1,13 +1,12 @@
 package com.telecom.asignaciones.Response;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
+
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 public class AsignacionResponse {
-    private Integer id;
+
 
     private String nombreSitio;
 
@@ -19,24 +18,114 @@ public class AsignacionResponse {
 
     private LocalDate fechaInicio;
 
-    @Column(name = "fecha_asignacion")
     private LocalDate fechaAsignacion;
 
-    @Column(name = "cordinador", length = Integer.MAX_VALUE)
     private String cordinador;
 
-    @Column(name = "lider", length = Integer.MAX_VALUE)
     private String lider;
 
-    @Column(name = "nomiembros", nullable = false)
     private Integer nomiembros;
 
-    @Column(name = "estado", length = Integer.MAX_VALUE)
     private String estado;
 
-    @Column(name = "estatus")
     private Boolean estatus;
 
-    @Column(name = "uuid_asignaciones", nullable = false)
     private UUID uuidAsignaciones;
+
+    public String getNombreSitio() {
+        return nombreSitio;
+    }
+
+    public void setNombreSitio(String nombreSitio) {
+        this.nombreSitio = nombreSitio;
+    }
+
+    public String getIdEnlace() {
+        return idEnlace;
+    }
+
+    public void setIdEnlace(String idEnlace) {
+        this.idEnlace = idEnlace;
+    }
+
+    public String getProyecto() {
+        return proyecto;
+    }
+
+    public void setProyecto(String proyecto) {
+        this.proyecto = proyecto;
+    }
+
+    public String getEscenario() {
+        return escenario;
+    }
+
+    public void setEscenario(String escenario) {
+        this.escenario = escenario;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaAsignacion() {
+        return fechaAsignacion;
+    }
+
+    public void setFechaAsignacion(LocalDate fechaAsignacion) {
+        this.fechaAsignacion = fechaAsignacion;
+    }
+
+    public String getCordinador() {
+        return cordinador;
+    }
+
+    public void setCordinador(String cordinador) {
+        this.cordinador = cordinador;
+    }
+
+    public String getLider() {
+        return lider;
+    }
+
+    public void setLider(String lider) {
+        this.lider = lider;
+    }
+
+    public Integer getNomiembros() {
+        return nomiembros;
+    }
+
+    public void setNomiembros(Integer nomiembros) {
+        this.nomiembros = nomiembros;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Boolean getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(Boolean estatus) {
+        this.estatus = estatus;
+    }
+
+    public UUID getUuidAsignaciones() {
+        return uuidAsignaciones;
+    }
+
+    public void setUuidAsignaciones(UUID uuidAsignaciones) {
+        this.uuidAsignaciones = uuidAsignaciones;
+    }
+
 }
--- src/main/java/com/telecom/asignaciones/controller/AsignacionController.java
+++ src/main/java/com/telecom/asignaciones/controller/AsignacionController.java
@@ -1,11 +1,14 @@
 package com.telecom.asignaciones.controller;
 
+import com.telecom.asignaciones.Response.AsignacionResponse;
 import com.telecom.asignaciones.service.AsignacionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -14,6 +17,8 @@ public class AsignacionController {
     @Autowired
     private AsignacionService asignacionService;
 
-    @GetMapping
-    public
+    @GetMapping("/asignacionesgit")
+    public List<AsignacionResponse> getAsignaciones() {
+        return AsignacionService.findAll();
+    }
 }
